5ce31cd5f992b254fc1c00f168ac305b52ac981d
Ankur Semle <semleankur@gmail.com>
2023-12-11 13:41:12 +0530

    add modal animations

---
+++ modal/animations/modalAnimation.tsx
@@ -0,0 +1,10 @@
+export const bgAnimation = {
+    closed: { opacity: 0 },
+    opened: { opactity: 1},
+};
+
+export const modalAnimation = {
+    closed: { y: -100 },
+    opened: { y: 0 },
+    exited: { y: 100 }
+}
