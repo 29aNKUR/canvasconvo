44f8afbab78baaabc7dd218426663ef80fbf8c54
Ankur Semle <semleankur@gmail.com>
2023-12-12 16:12:50 +0530

    add portal file

---
+++ modal/components/modalManager.tsx
@@ -0,0 +1,13 @@
+import { motion, AnimatePresence } from "framer-motion"
+
+export const MyComponent = ({ isVisible }) => (
+  <AnimatePresence>
+    {isVisible && (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      />
+    )}
+  </AnimatePresence>
+)
