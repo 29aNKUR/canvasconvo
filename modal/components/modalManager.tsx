4ff76118ef50704bab7188d9b579c5fe4197470d
Ankur Semle <semleankur@gmail.com>
2023-12-14 12:14:54 +0530

    modalManager

--- modal/components/modalManager.tsx
+++ modal/components/modalManager.tsx
@@ -1,31 +1,54 @@
 // import { motion, AnimatePresence } from "framer-motion"
 
+import Portal from "@/common/components/portal/portal";
 import { useEffect, useState } from "react";
-
-// export const MyComponent = ({ isVisible }) => (
-//   <AnimatePresence>
-//     {isVisible && (
-//       <motion.div
-//         initial={{ opacity: 0 }}
-//         animate={{ opacity: 1 }}
-//         exit={{ opacity: 0 }}
-//       />
-//     )}
-//   </AnimatePresence>
-// )
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { modalAtom } from "../recoil/modal.atom";
+import { motion, AnimatePresence } from "framer-motion";
+import { bgAnimation, modalAnimation } from "../animations/modalAnimation";
 
 const modalManager = () => {
+  const [portalNode, setPortalNode] = useState<HTMLElement | null>(null);
 
-    const [portalNode, setPortalNode] = useState();
+  const [{ opened, modal }, setModal] = useRecoilState(modalAtom);
 
-    useEffect(() => {
-        if(!portalNode){
-            const node = document.getElementById("portal")
-            if(node) setPortalNode(node)
-            return;
-        }
-    })
+  useEffect(() => {
+    const node = document.getElementById("portal");
+    if (node) setPortalNode(node);
+  }, []);
 
-}
+  const closeAndStopPropagation = () => {
+    setModal({ modal: <></>, opened: false });
+  };
+
+  return (
+    <div>
+      {opened && (
+        <motion.div
+          className="absolute z-40 flex min-h-full w-full items-center justify-center bg-black/80"
+          variants={bgAnimation}
+          onClick={closeAndStopPropagation}
+          initial="closed"
+          animate="opened"
+        >
+          <AnimatePresence>
+            {opened && (
+              <motion.div
+                className="p-6"
+                variants={modalAnimation}
+                initial="closed"
+                animate="opened"
+                exit="exited"
+                onClick={(e) => e.stopPropagation}
+              >
+                {modal}
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </motion.div>
+      )}
+    </div>
+  );
+};
 
 export default modalManager;
