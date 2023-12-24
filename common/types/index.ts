a84d74aca2f0143035408f430975fbad37e35cb7
Ankur Semle <semleankur@gmail.com>
2023-12-24 18:58:37 +0530

    add global types

--- common/types/index.ts
+++ common/types/index.ts
@@ -42,3 +42,51 @@ export type Room = {
     drawed: Move[];
     users: Map<string, string>;
 }
+
+export interface User {
+    name: string;
+    color: string;
+}
+
+export interface clientRoom {
+    id: string;
+    userMoves: Map<string, Move[]>;
+    movesWithoutUser: Move[];
+    myMoves: Move[];
+    users: Map<string, User>; 
+}
+
+export interface MessageType {
+    userId: string;
+    username: string;
+    color: string;
+    msg: string;
+    id: number;
+}
+
+export interface ServerToClientEvents {
+    room_exists: (exists: boolean) => void;
+    joined: (roomId: string, failed?: boolean) => void;
+    room: (room: Room, usersMovesToParse: string, usersToParse: string) => void;
+    created: (roomId: string) => void;
+    your_move: (move: Move) => void;
+    user_draw: (move: Move, userId: string) => void;
+    user_undo(userId: string): void;
+    mouse_moved: (x: number, y: number, userId: string) => void;
+    new_user: (userId: string, username: string) => void;
+    user_disconnected: (userId: string) => void;
+    new_msg: (userId: string, msg: string) => void;
+  }
+  
+  export interface ClientToServerEvents {
+    check_room: (roomId: string) => void;
+    draw: (move: Move) => void;
+    mouse_move: (x: number, y: number) => void;
+    undo: () => void;
+    create_room: (username: string) => void;
+    join_room: (room: string, username: string) => void;
+    joined_room: () => void;
+    leave_room: () => void;
+    send_msg: (msg: string) => void;
+  }
+  
