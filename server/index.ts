21042f17a5543304ea1ff225f88d0ebffd0596f7
Ankur Semle <semleankur@gmail.com>
2023-12-25 23:50:07 +0530

    add create room logic in server

--- server/index.ts
+++ server/index.ts
@@ -16,6 +16,7 @@ const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const nextHandler: NextApiHandler = nextApp.getRequestHandler();
 
+//Socket.IO connection handing
 nextApp.prepare().then(() => {
   const app = express();
   const server = createServer(app);
@@ -53,6 +54,7 @@ nextApp.prepare().then(() => {
     room.usersMoves.get(socketId)!.pop();
   };
 
+  //Socket.IO Connection Event Handling
   io.on("connection", (socket) => {
     /*getRoomId is a function defined within the connection event. It is responsible for determining the room identifier associated with the current socket. */
     const getRoomId = () => {
@@ -83,9 +85,39 @@ nextApp.prepare().then(() => {
 
       /*This line removes the user (socketId) from the users Map within the room object. The assumption here is that each user is associated with a unique socket ID. */
       room.users.delete(socketId);
-        /* this line instructs the socket associated with this operation to leave the specified Socket.IO room (roomId). This is a Socket.IO method that manages rooms and helps organize sockets. */
+      /* this line instructs the socket associated with this operation to leave the specified Socket.IO room (roomId). This is a Socket.IO method that manages rooms and helps organize sockets. */
       socket.leave(roomId);
     };
+
+    //'create_room' Event Handling
+    socket.on("create_room", (username) => {
+      // Generate a unique room ID
+      let roomId: string;
+      /*The do-while loop generates a random string (roomId) using Math.random() and toString(36) functions. The substring(2, 6) extracts a portion of the string, creating a short identifier.
+The loop ensures that the generated roomId is unique by checking if it already exists in the rooms map. If it does, the loop generates a new roomId until a unique one is found */
+      do {
+        roomId = Math.random().toString(36).substring(2, 6);
+      } while (rooms.has(roomId));
+
+      //Join the room
+      /*The socket.join(roomId) method makes the current socket (user) join the room with the newly created roomId. This is a Socket.IO feature that allows a socket to be part of one or more "rooms." */
+      socket.join(roomId);
+
+      //Create and Add a new Room object to the 'rooms' Map
+      /*A new room object is created with three properties:
+usersMoves: A Map containing the current user's socket ID mapped to an empty array. This array will store the drawing moves made by the users in the room.
+drawed: An empty array that will store the collective drawing made by all users in the room.
+users: A Map containing the current user's socket ID mapped to their username. */
+      rooms.set(roomId, {
+        usersMoves: new Map([[socket.id, []]]),
+        drawed: [],
+        users: new Map([[socket.id, username]]),
+      });
+
+      //Emit a 'created' event
+      /*The io.to(socket.id).emit('created', roomId) emits a 'created' event to the current user, providing them with the newly created roomId. This allows the client to know the ID of the room they have just created. */
+      io.to(socket.id).emit("created", roomId);
+    });
   });
 });
 
