99f31511652bd19195b81e74267faaea4ba864bc
Ankur Semle <semleankur@gmail.com>
2023-12-28 22:40:07 +0530

    correct quotes

--- modules/home/modals/NotFound.tsx
+++ modules/home/modals/NotFound.tsx
@@ -8,13 +8,13 @@ const NotFoundModal = ({ id }: { id: string }) => {
         <div className='relative flex flex-col items-center rounded-md bg-white p-10'>
             <button onClick={closeModal} className='absolute top-5 right-5'>
                 <AiOutlineClose />
-                </button>   
-                <h2 className='text-lg font-bold'>
-                    Room with id &qout;{id}&quot; does not exist or is full!
-                </h2>
-                <h3>Try to join room later</h3>
+            </button>   
+            <h2 className='text-lg font-bold'>
+                Room with id &quot;{id}&quot; does not exist or is full!
+            </h2>
+            <h3>Try to join the room later</h3>
         </div>
-    )
+    );
 }
 
 export default NotFoundModal;
