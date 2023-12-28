72b71e1336858f2b14617cf7bd22ff4b68e053a0
Ankur Semle <semleankur@gmail.com>
2023-12-28 22:38:03 +0530

    add notfound modal

--- modules/home/modals/NotFound.tsx
+++ modules/home/modals/NotFound.tsx
@@ -5,6 +5,16 @@ const NotFoundModal = ({ id }: { id: string }) => {
     const { closeModal } = useModal();
 
     return (
-        <div className='relative flex flex-col items-center rounded-md bg-white p-10'></div>
+        <div className='relative flex flex-col items-center rounded-md bg-white p-10'>
+            <button onClick={closeModal} className='absolute top-5 right-5'>
+                <AiOutlineClose />
+                </button>   
+                <h2 className='text-lg font-bold'>
+                    Room with id &qout;{id}&quot; does not exist or is full!
+                </h2>
+                <h3>Try to join room later</h3>
+        </div>
     )
 }
+
+export default NotFoundModal;
