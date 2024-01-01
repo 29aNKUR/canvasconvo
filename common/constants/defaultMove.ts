9a37e48c7c8541e843d23c982cf1443b83480a2e
Ankur Semle <semleankur@gmail.com>
2024-01-01 23:18:46 +0530

    add default move

---
+++ common/constants/defaultMove.ts
@@ -0,0 +1,28 @@
+import { Move } from "../types/global";
+
+export const DEFAULT_MOVE: Move = {
+    circle: {
+        cX: 0,
+        cY: 0,
+        radiusX: 0,
+        radiusY: 0,
+    },
+    rect: {
+        width: 0,
+        height: 0,
+    },
+    path: [],
+    options: {
+        shape: "line",
+        mode: 'draw',
+        lineWidth: 1,
+        lineColor: { r: 0, g: 0, b: 0, a: 0 },
+        fillColor: { r: 0, g: 0, b: 0, a: 0 },
+        selection: null,
+    },
+    id: '',
+    img: {
+        base64: '',
+    },
+    timestamp: 0,
+};
