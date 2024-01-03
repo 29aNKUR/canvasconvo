c8ed1289a2ebd70a78c6d1cb67e8eb0fc66b8ac1
Ankur Semle <semleankur@gmail.com>
2024-01-04 02:15:40 +0530

    add room atom

--- common/recoil/options/options.hooks.ts
+++ common/recoil/options/options.hooks.ts
@@ -2,24 +2,28 @@ import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 
 import { optionsAtom } from './options.atom';
 
+//This hook uses useRecoilValue to access the current value of the optionsAtom. It returns the options object.
 export const useOptionsValue = () => {
   const options = useRecoilValue(optionsAtom);
 
   return options;
 };
 
+//This hook uses useSetRecoilState to get the function to set the state of optionsAtom. It returns this function (setOptions), which can be used to update the state.
 export const useSetOptions = () => {
   const setOptions = useSetRecoilState(optionsAtom);
 
   return setOptions;
 };
 
+//This hook uses useRecoilState to get both the current value of optionsAtom and the function to set its state. It returns an array with these two value
 export const useOptions = () => {
   const options = useRecoilState(optionsAtom);
 
   return options;
 };
 
+//This hook uses the useSetOptions hook to obtain the setOptions function. It provides two functions - setSelection and clearSelection. setSelection takes a rectangle object and updates the selection property in the options, while clearSelection sets selection to null
 export const useSetSelection = () => {
   const setOptions = useSetOptions();
 
---
+++ common/recoil/room/room.atom.ts
@@ -0,0 +1,16 @@
+import { atom } from "recoil";
+
+import { ClientRoom } from "@/common/types/global";
+
+export const DEFAULT_ROOM = {
+    id: '',
+    users: new Map(),
+    usersMoves: new Map(),
+    movesWithoutUser: [],
+    myMoves: [],
+}
+
+export const roomAtom = atom<ClientRoom>({
+    key: 'room',
+    default: DEFAULT_ROOM,
+})
