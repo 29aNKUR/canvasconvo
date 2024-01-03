28db38989b1347f8cfbb60a1e718740831d7f567
Ankur Semle <semleankur@gmail.com>
2024-01-03 16:15:33 +0530

    add useBackground hook

--- store/store.ts
+++ store/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
  import roomSlice from "@/common/redux/room/roomSlice";
 import modalSlice from "@/modal/redux/modalSlice";
+import backgroundSlice from "@recoil/background/backgroundSlice";
 
 
  const store = configureStore({
     reducer: {
         room: roomSlice,
-        modal: modalSlice
+        modal: modalSlice,
+        background: backgroundSlice
     },
  });
 
