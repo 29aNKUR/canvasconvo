1b4df1b033427d2343aa60376919a9208ce2a9ff
Ankur Semle <semleankur@gmail.com>
2024-01-05 02:07:40 +0530

    update _app _document

--- common/recoil/room/room.hooks.ts
+++ common/recoil/room/room.hooks.ts
@@ -1,39 +1,99 @@
-import { useDispatch, useSelector } from "react-redux";
-import { setRoom } from "./roomSlice";
+import { useRecoilValue, useSetRecoilState } from "recoil"
+import { DEFAULT_ROOM, roomAtom } from "./room.atom"
+import { getNextColor } from "@/common/lib/getNextColor";
+import { Move } from "@/common/types/global";
 
-
+//This hook provides read-only access to the current state of the roomAtom.
 export const useRoom = () => {
-    const room = useSelector((state: any) => state.room);
+    const room = useRecoilValue(roomAtom);
+
     return room;
-}
+};
 
+//This hook provides the setRoom function, which can be used to update the state of the roomAtom.
 export const useSetRoom = () => {
-    const dispatch = useDispatch();
+    const setRoom = useSetRecoilState(roomAtom);
 
-    const setRoomFunc = (newRoom: any) => {
-        dispatch(setRoom({newRoom}));
-    }
-    return setRoomFunc;
-}
+    return setRoom;
+};
 
+//This hook provides a function handleSetRoomId that can be used to update the room's id while preserving other default values.
+//In a component, we can call useSetRoomId() to get the handleSetRoomId function.
 export const useSetRoomId = () => {
-    const dispatch = useDispatch();
+    const setRoomId = useSetRecoilState(roomAtom);
 
     const handleSetRoomId = (id: string) => {
-        dispatch(setRoom((prev: any) => ({
-            ...prev,
-            id,
-        })));
+        setRoomId({...DEFAULT_ROOM, id});
     };
 
     return handleSetRoomId;
 }
 
-// export const useSetUsers = () => {
-//     const dispatch = useDispatch();
+//This hook provides functions (handleAddUser, handleRemoveUser, handleAddMoveToUser, handleRemoveMoveFromUser) for manipulating the users and usersMoves properties in the room.
+export const useSetUsers = () => {
+    const setRoom = useSetRecoilState(roomAtom);
 
-//     const handleAddUser = (userId: string, name: string) => {
-//         dispatch(setRoom)
-//     }
-// }
+    const handleAddUser = (userId: string, name: string) => {
+        setRoom((prev) => {
+            const newUsers = prev.users;
+            const newUsersMoves = prev.usersMoves;
 
+            const color = getNextColor([...newUsers.values()].pop()?.color);
+            
+            newUsers.set(userId, {
+                name,
+                color,
+            });
+            newUsersMoves.set(userId, []);
+        
+            return { ...prev, users: newUsers, usersMoves: newUsersMoves};
+        });
+    };
+
+    const handleRemoveUser = (userId: string) => {
+        setRoom((prev) => {
+            const newUsers = prev.users;
+            const newUsersMoves = prev.usersMoves;
+
+            const usersMoves = newUsersMoves.get(userId);
+
+            newUsers.delete(userId);
+            newUsersMoves.delete(userId);
+            return {
+                ...prev,
+                users: newUsers,
+                usersMoves: newUsersMoves,
+                movesWithoutUser: [ ...prev.movesWithoutUser, ...(usersMoves || [])],
+            };   
+        });
+    };
+
+    const handleAddMoveToUser = (userId: string, moves: Move) => {
+        setRoom((prev) => {
+            const newUsersMoves = prev.usersMoves;
+            const oldMoves = prev.usersMoves.get(userId);
+
+            newUsersMoves.set(userId, [...(oldMoves || []), moves]);
+            return { ...prev, usersMoves: newUsersMoves };
+        });
+    };
+
+    const handleRemoveMoveFromUser = (userId: string) => {
+        setRoom((prev) => {
+            const newUsersMoves = prev.usersMoves;
+            const oldMoves = prev.usersMoves.get(userId);
+            oldMoves?.pop();
+
+            newUsersMoves.set(userId, oldMoves || []);
+            return { ...prev, usersMoves: newUsersMoves };
+        });
+    };
+
+    return {
+        handleAddUser,
+        handleRemoveUser,
+        handleAddMoveToUser,
+        handleRemoveMoveFromUser
+    };
+};
+ 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
+import { DEFAULT_EASE } from '@/common/constants/easings'
+import { ModalManager } from '@/modal'
 import '@/styles/globals.css'
+import { MotionConfig } from 'framer-motion'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ToastContainer } from 'react-toastify'
+import { RecoilRoot } from 'recoil'
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+    <title>CanvasConvo | Onlie Whiteboard with Chat</title>
+    <link rel="icon" href="/favicon.ico"/>
+    </Head>
+    <RecoilRoot>
+      <ToastContainer />
+      <MotionConfig transition={{ ease: DEFAULT_EASE }}>
+        <ModalManager />
+        <Component {...pageProps} />
+        </MotionConfig>
+    </RecoilRoot>
+    </>
+  )
 }
