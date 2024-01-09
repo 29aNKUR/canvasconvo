f63d9788d448d135c1a61306b0414016deac1b65
Ankur Semle <semleankur@gmail.com>
2024-01-09 15:58:11 +0530

    add userList comp

--- modules/room/components/UserList.tsx
+++ modules/room/components/UserList.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useRoom } from "@recoil/room";
 
 const UserList = () => {
+  const { users } = useRoom();
   return (
-    <div>UserList</div>
-  )
-}
+    <div>
+      {[...users.keys()].map((userId, index) => {
+        return (
+          <div
+            key={userId}
+            className="flex h-5 w-5 select-none items-center justify-center
+            rounded-full text-xs text-white md:h-8 md:w-8 md:text-base lg:h-12
+            lg:w-12"
+            style={{
+                backgroundColor: users.get(userId)?.color || 'black',
+                marginLeft: index !== 0 ? '-0.5rem' : 0,
+            }}
+         >
+            {users.get(userId)?.name.split('')[0] || 'A'}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default UserList
+export default UserList;
