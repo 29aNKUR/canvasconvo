98c9dade1491297839a575ad4a45664db12cc463
Ankur Semle <semleankur@gmail.com>
2024-01-14 00:51:18 +0530

    add Message component

--- modules/room/modules/board/hooks/useDraw.ts
+++ modules/room/modules/board/hooks/useDraw.ts
@@ -1,4 +1,4 @@
-import { useOptionsValue, useSetSelection } from "@/common/recoil/options"
+import { useOptionsValue, useSetSelection } from "@/common/recoil/options";
 import { useBoardPosition } from "./useBoardPosition";
 import { useMyMoves } from "@/common/recoil/room";
 import { useSetSavedMoves } from "@/common/recoil/savedMoves";
@@ -6,50 +6,77 @@ import { useViewportSize } from "@/common/hooks/useViewportSize";
 import { useState } from "react";
 import { useCtx } from "./useCtx";
 import { getStringFromRgba } from "@/common/lib/rgba";
+import { getPos } from "@/common/lib/getPos";
 
 let tempMoves: [number, number][] = [];
 let tempCircle = { cX: 0, cY: 0, radiusX: 0, radiusY: 0 };
 let tempSize = { width: 0, heigth: 0 };
 let tempImageData: ImageData | undefined;
 
-
 export const useDraw = (blocked: boolean) => {
-    const options = useOptionsValue();
-    const boardPosition = useBoardPosition();
-    const { clearSavedMoves } = useSetSavedMoves();
-    const { handleAddMyMove } = useMyMoves();
-    const { setSelection, clearSelection } = useSetSelection();
-    const vw = useViewportSize();
+  const options = useOptionsValue();
+  const boardPosition = useBoardPosition();
+  const { clearSavedMoves } = useSetSavedMoves();
+  const { handleAddMyMove } = useMyMoves();
+  const { setSelection, clearSelection } = useSetSelection();
+  const vw = useViewportSize();
 
-    const movedX = boardPosition.x;
-    const movedY = boardPosition.y;
+  const movedX = boardPosition.x;
+  const movedY = boardPosition.y;
 
-    const [drawing, setDrawing] = useState(false);
-    const ctx = useCtx();
+  const [drawing, setDrawing] = useState(false);
+  const ctx = useCtx();
 
-    const setupCtxOptions = () => {
-        if (ctx) {
-            ctx.lineWidth = options.lineWidth;
-            ctx.strokeStyle = getStringFromRgba(options.lineColor);
-            ctx.fillStyle = getStringFromRgba(options.fillColor);
-            if(options.mode === 'eraser')
-            ctx.globalCompositeOperation='destination-out';
-        else ctx.globalCompositeOperation = 'source-over'; 
-        }
-    };
+  const setupCtxOptions = () => {
+    if (ctx) {
+      ctx.lineWidth = options.lineWidth;
+      ctx.strokeStyle = getStringFromRgba(options.lineColor);
+      ctx.fillStyle = getStringFromRgba(options.fillColor);
+      if (options.mode === "eraser")
+        ctx.globalCompositeOperation = "destination-out";
+      else ctx.globalCompositeOperation = "source-over";
+    }
+  };
 
-    const drawAndSet = () => {
-        if(!tempImageData)
-        tempImageData = ctx?.getImageData(
-    movedX.get() * -1,
-    movedY.get() * -1,
-    vw.width,
-    vw.height
-    );
+  const drawAndSet = () => {
+    if (!tempImageData)
+      tempImageData = ctx?.getImageData(
+        movedX.get() * -1,
+        movedY.get() * -1,
+        vw.width,
+        vw.height
+      );
 
-    if(tempImageData)
-    ctx?.putImageData(tempImageData, movedX.get() * -1, movedY.get() * -1);
+    if (tempImageData)
+      ctx?.putImageData(tempImageData, movedX.get() * -1, movedY.get() * -1);
+  };
+
+  const handleStartDrawing = (x: number, y: number) => {
+    if(!ctx || blocked || blocked) return;
+
+    const [finalX, finalY] = [getPos(x, movedX), getPos(y, movedY)];
+
+    setDrawing(true);
+    setupCtxOptions();
+    drawAndSet();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
-
-
-}
---
+++ modules/room/modules/chat/components/Message.tsx
@@ -0,0 +1,21 @@
+import { socket } from "@/common/lib/socket";
+import { MessageType } from "@/common/types/global";
+
+const Message = ({ userId, msg, username, color }: MessageType) => {
+    //check If the message is sent by the current user    
+    const me = socket.id === userId;
+
+    return (
+        <div className={`my-2 flex gap-2 text-clip ${me && 'justify-end text-right'}`}>
+        {/* Display the username if the message is not from the current user */}
+        {!me && (
+            <h5 style = {{ color }} className="font-bold">
+                {username}
+            </h5>
+        )}
+        <p style={{ wordBreak: 'break-all'}}>{msg}</p>
+        </div>
+    );
+};
+
+export default Message;
