bb399a30cf3e8e8514cc7469bb77ff0dfcca524c
Ankur Semle <semleankur@gmail.com>
2024-01-16 23:57:10 +0530

    add modepicker comp

--- modules/room/modules/toolbar/components/ModePicker.tsx
+++ modules/room/modules/toolbar/components/ModePicker.tsx
@@ -1,6 +1,53 @@
+import { useOptions, useSetSelection } from "@/common/recoil/options";
+import { useEffect } from "react";
+import { AiOutlineSelect } from "react-icons/ai";
+import { BsPencilFill } from "react-icons/bs";
+import { FaEraser } from "react-icons/fa";
 
 const ModePicker = () => {
+  const [options, setOptions] = useOptions();
+  const { clearSelection } = useSetSelection();
 
-}
+  useEffect(() => {
+    clearSelection();
+  }, [options.mode]);
+
+  return (
+    <>
+      <button
+        className={`btn-icon text-xl ${
+          options.mode === "draw" && "bg-green-400"
+        }`}
+        onClick={() => {
+          setOptions((prev) => ({
+            ...prev,
+            mode: "draw",
+          }));
+        }}
+      >
+        <BsPencilFill />
+      </button>
+
+      <button className={`btn-icon text-xl ${options.mode === 'eraser' && 'bg-green-400'}`} onClick={() => {
+        setOptions((prev) => ({
+            ...prev,
+            mode: 'eraser',
+        }));
+      }}
+      >
+        <FaEraser />
+      </button>
+
+      <button className={`btn-icon text-2xl ${options.mode === 'select' && 'bg-green-400'}`} onClick={() => {
+        setOptions((prev) => ({
+            ...prev,
+            mode: 'select',
+        }));
+      }}>
+        <AiOutlineSelect />
+      </button>
+    </>
+  );
+};
 
 export default ModePicker;
