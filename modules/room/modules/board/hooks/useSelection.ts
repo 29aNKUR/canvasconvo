b4db89b00da24b34673d41b05f74fc336b43e036
Ankur Semle <semleankur@gmail.com>
2024-01-25 23:41:34 +0530

    useSelection cp2

--- modules/room/modules/board/hooks/useSelection.ts
+++ modules/room/modules/board/hooks/useSelection.ts
@@ -3,6 +3,9 @@ import { useCtx } from "./useCtx";
 import { useRefs } from "@/modules/room/hooks/useRefs";
 import { useMoveImage } from "@/modules/room/hooks/useMoveImage";
 import { useEffect, useMemo } from "react";
+import { Move } from "@/common/types/global";
+import { socket } from "@/common/lib/socket";
+import { DEFAULT_MOVE } from "@/common/constants/defaultMove";
 
 
 let tempSelection = {
@@ -100,9 +103,68 @@ export const useSelection = (drawAllMoves: () => Promise<void>) => {
             canvas.width = width;
             canvas.height = height;
             const tempCtx = canvas.getContext('2d');
+
+            if (tempCtx && bgRef.current) {
+                const bgImage = bgRef.current.getContext('2d')?.getImageData(x, y, width, height);
+
+                if(bgImage && withBg) tempCtx.putImageData(bgImage, 0, 0);
+
+                const sTempCtx = tempCanvas.getContext('2d');
+                sTempCtx?.putImageData(imageData, 0, 0);
+
+                tempCtx.drawImage(tempCanvas, 0, 0);
+
+                const blob: Blob = await new Promise((resolve) => {
+                    canvas.toBlob((blobGenerated) => {
+                        if (blobGenerated) resolve(blobGenerated);
+                    });
+                });
+
+                return blob;
+            }
+        }
+        return null;
+    };
+
+    const createDeleteMove = () => {
+        if(!selection) return null;
+
+        let { x, y, width, height} = dimension;
+
+        if(width < 0) {
+            width += 4;
+            x -= 2;
+        } else {
+            width -= 4;
+            x += 2;
+        }
+        if (height < 0) {
+            height += 4;
+            y -= 2;
+        } else {
+            height -= 4;
+            y += 2;
         }
 
-    }
+        const move: Move = {
+            ...DEFAULT_MOVE,
+            rect: {
+                width,
+                height,
+            },
+            path: [[x, y]],
+            options: {
+                ...options,
+                shape: 'rect',
+                mode: 'eraser',
+                fillColor: { r: 0, g: 0, b: 0, a: 1 },
+            },
+        };
+
+        socket.emit('draw', move);
+
+        return move;
+    };
 
 
 }
