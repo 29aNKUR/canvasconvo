60712cb47bb08d5e989feb094f5e32acab3f7b10
Ankur Semle <semleankur@gmail.com>
2024-01-29 23:48:15 +0530

    useMovesHandlers cp

--- modules/room/hooks/useMovesHandlers.ts
+++ modules/room/hooks/useMovesHandlers.ts
@@ -1,45 +1,72 @@
 import { useMyMoves, useRoom } from "@/common/recoil/room";
-import { useRefs } from "./useRefs"
+import { useRefs } from "./useRefs";
 import { useSetSavedMoves } from "@/common/recoil/savedMoves";
 import { useCtx } from "../modules/board/hooks/useCtx";
 import { useBackground } from "@/common/recoil/background";
 import { useSetSelection } from "@/common/recoil/options";
 import { useEffect, useMemo } from "react";
+import { Move } from "@/common/types/global";
+import { getStringFromRgba } from "@/common/lib/rgba";
 
 let prevMovesLength = 0;
 
 export const useMovesHandlers = (clearOnYourMove: () => void) => {
-    const { canvasRef, minimapRef, bgRef } = useRefs();
-    const room = useRoom();
-    const { handleAddMyMove, handleRemoveMyMove } = useMyMoves();
-    const { addSavedMove, removeSavedMove } = useSetSavedMoves();
-    const ctx = useCtx();
-    const bg = useBackground();
-    const { clearSelection } = useSetSelection();
+  const { canvasRef, minimapRef, bgRef } = useRefs();
+  const room = useRoom();
+  const { handleAddMyMove, handleRemoveMyMove } = useMyMoves();
+  const { addSavedMove, removeSavedMove } = useSetSavedMoves();
+  const ctx = useCtx();
+  const bg = useBackground();
+  const { clearSelection } = useSetSelection();
 
-    const sortedMoves = useMemo(() => {
-        const { usersMoves, movesWithoutUser, myMoves } = room;
+  const sortedMoves = useMemo(() => {
+    const { usersMoves, movesWithoutUser, myMoves } = room;
 
-        const moves = [...movesWithoutUser, ...myMoves];
+    const moves = [...movesWithoutUser, ...myMoves];
 
-        usersMoves.forEach((userMoves) => moves.push(...userMoves));
+    usersMoves.forEach((userMoves) => moves.push(...userMoves));
 
-        moves.sort((a, b) => a.timestamp - b.timestamp);
+    moves.sort((a, b) => a.timestamp - b.timestamp);
 
-        return moves;
-    }, [room]);
+    return moves;
+  }, [room]);
 
-    const copyCanvasToSmall = () => {
-        if(canvasRef.current && minimapRef.current && bgRef.current) {
-            const smallCtx = minimapRef.current.getContext('2d');
-            if(smallCtx) {
-                smallCtx.clearRect(0, 0, smallCtx.canvas.width, smallCtx.canvas.height);
-                smallCtx.drawImage(bgRef.current, 0, 0, smallCtx.canvas.width, smallCtx.canvas.height);
-                smallCtx.drawImage(canvasRef.current, 0, 0, smallCtx.canvas.width, smallCtx.canvas.height);
-            }
-        }
-    };
+  const copyCanvasToSmall = () => {
+    if (canvasRef.current && minimapRef.current && bgRef.current) {
+      const smallCtx = minimapRef.current.getContext("2d");
+      if (smallCtx) {
+        smallCtx.clearRect(0, 0, smallCtx.canvas.width, smallCtx.canvas.height);
+        smallCtx.drawImage(
+          bgRef.current,
+          0,
+          0,
+          smallCtx.canvas.width,
+          smallCtx.canvas.height
+        );
+        smallCtx.drawImage(
+          canvasRef.current,
+          0,
+          0,
+          smallCtx.canvas.width,
+          smallCtx.canvas.height
+        );
+      }
+    }
+  };
 
-    useEffect(() => copyCanvasToSmall(), [bg]);
+  useEffect(() => copyCanvasToSmall(), [bg]);
 
-}
+  const drawMove = (move: Move, image?: HTMLImageElement) => {
+    const { path } = move;
+
+    if(!ctx || !path.length) return;
+
+    const moveOptions = move.options;
+
+    if(moveOptions.mode === 'select') return;
+
+    ctx.lineWidth = moveOptions.lineWidth;
+    ctx.strokeStyle = getStringFromRgba(moveOptions.lineColor);
+    ctx.fillStyle = getStringFromRgba(moveOptions.fillColor);
+  }
+};
