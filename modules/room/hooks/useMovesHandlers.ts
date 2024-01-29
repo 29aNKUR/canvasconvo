202efd7b6386ad6f92503fe34aea0e427fbac491
Ankur Semle <semleankur@gmail.com>
2024-01-30 00:48:20 +0530

    useMovesHandlers cp2

--- modules/room/hooks/useMovesHandlers.ts
+++ modules/room/hooks/useMovesHandlers.ts
@@ -68,5 +68,59 @@ export const useMovesHandlers = (clearOnYourMove: () => void) => {
     ctx.lineWidth = moveOptions.lineWidth;
     ctx.strokeStyle = getStringFromRgba(moveOptions.lineColor);
     ctx.fillStyle = getStringFromRgba(moveOptions.fillColor);
-  }
+
+    if(moveOptions.mode === 'eraser') {
+        ctx.globalCompositeOperation = 'destination-out';
+    } else {
+        ctx.globalCompositeOperation = 'source-over';
+    }
+
+    if(moveOptions.shape === 'image' && image) {
+        ctx.drawImage(image,path[0][0], path[0][1]);
+    }
+
+    switch (moveOptions.shape) {
+        case 'line' : {
+            ctx.beginPath();
+            path.forEach(([x, y]) => {
+                ctx.lineTo(x, y);
+            });
+
+            ctx.stroke();
+            ctx.closePath();
+            break;
+        }
+
+        case 'circle' : {
+            const { cX, cY, radiusX, radiusY } = move.circle;
+
+            ctx.beginPath();
+            ctx.ellipse(cX, cY, radiusX, radiusY, 0, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.fill();
+            ctx.closePath();
+            break;
+        }
+
+        case 'rect' : {
+            const { width, height } = move.rect;
+
+            ctx.beginPath();
+
+            ctx.rect(path[0][0], path[0][1], width, height);
+            ctx.stroke();
+            ctx.fill();
+
+            ctx.closePath();
+            break;
+        }
+
+        default:
+            break;
+    }
+
+    copyCanvasToSmall();
+  };
+
+  
 };
